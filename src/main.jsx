402e708ebf1b19a7290f93ebd1805588213b7981
Mohammad <m.mahdi.r.1400@gmail.com>
2026-02-27 16:14:18 +0330

    I renamed App.jasx to Todo_app.jsx, and I also modified the project code to provide it with an interface that allows deleting, editing, and changing the status.

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import App from './App.jsx'
+
+import Todo from './Todo_app.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Todo />
   </StrictMode>,
 )
